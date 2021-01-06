763173d7b5ec53581c7e57e504bd2cd9f5177149
Umar--Khan <umarkhandev@gmail.com>
2021-01-06 13:57:15 +0000

    Functionality to render stories

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 const BASE_URL = "http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test";
 
 const NYTD = {
-  render_section_front: (something) => {
-    console.log(something);
+  render_section_front: (data) => {
+    generateContent(data);
   },
 };
 
@@ -13,10 +13,40 @@ function fetchNyRegionData() {
   );
 }
 
-async function extractData() {
+async function extractDataFromResponse() {
   const response = await fetchNyRegionData();
   const F = new Function(response);
   F();
 }
 
-extractData();
+extractDataFromResponse();
+
+function generateCard(newsArticle) {
+  const storiesContent = document.querySelector(".stories__content");
+  const card = document.createElement("div");
+  card.className = "stories__content__news__article__card";
+
+  if (newsArticle.headline) {
+    card.innerHTML = `
+    <div>
+        <p>${newsArticle.lastPublished}</p>
+        <h1>${newsArticle.headline}</h1>
+        <p>${newsArticle.summary}</p>
+        <p>${newsArticle.byline}</p>
+        <hr />
+    </div>`;
+    storiesContent.appendChild(card);
+  }
+}
+
+function generateContent(data) {
+  const content = data.page.content;
+
+  content.map((contentItem) => {
+    contentItem.collections.map((collection) => {
+      if (collection.assets.length >= 1) {
+        collection.assets.map((asset) => generateCard(asset));
+      }
+    });
+  });
+}
