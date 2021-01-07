b14c2d60984f77b5def2696ad88dc57a664df374
Umar--Khan <umarkhandev@gmail.com>
2021-01-07 12:54:24 +0000

    Added base css styling - cleanup on date format - other refactors

--- js/index.js
+++ js/index.js
@@ -3,43 +3,62 @@ const BASE_URL = "http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test";
 
 const NYTD = {
   render_section_front: (data) => {
-    generateContent(data);
+    generateCardsContent(data);
   },
 };
 
-function fetchNyRegionData() {
-  return fetch(`${PROXY_URL + BASE_URL}/${"nyregion.js"}`).then((resp) =>
-    resp.text()
-  );
+function fetchNyTestData(url) {
+  return fetch(`${PROXY_URL + BASE_URL}/${url}`).then((resp) => resp.text());
 }
 
-async function extractDataFromResponse() {
-  const response = await fetchNyRegionData();
+async function extractedDataFromNyRegion() {
+  const response = await fetchNyTestData("nyregion.js");
   const F = new Function(response);
   F();
 }
 
-extractDataFromResponse();
+extractedDataFromNyRegion();
 
 function generateCard(newsArticle) {
-  const storiesContent = document.querySelector(".stories__content");
+  const storiesContent = document.querySelector(".content");
   const card = document.createElement("div");
-  card.className = "stories__content__news__article__card";
+  card.className = "card border__bottom";
 
-  if (newsArticle.headline) {
+  const { lastPublished, headline, summary, byline, type } = newsArticle;
+
+  if (type !== "HTML" && lastPublished && headline && summary && byline) {
+    const lastPublishedDate = newsArticle.lastPublished.substr(
+      0,
+      lastPublished.indexOf("T")
+    );
+    let lastPublishedDateFormatted = new Date(lastPublishedDate);
+    const options = { month: "long", day: "numeric", year: "numeric" };
+
+    lastPublishedDateFormatted = lastPublishedDateFormatted.toLocaleDateString(
+      "EN-us",
+      options
+    );
+
     card.innerHTML = `
     <div>
-        <p>${newsArticle.lastPublished}</p>
-        <h1>${newsArticle.headline}</h1>
-        <p>${newsArticle.summary}</p>
-        <p>${newsArticle.byline}</p>
-        <hr />
+        <span class="tag">${lastPublishedDateFormatted}</span>
+        <div class="card__content">
+          <div class="card__left">
+            <h2>${headline}</h2>
+            <p>${summary}</p>
+          </div>
+          <div class="card__right">
+            <img class="card__img" src="https://via.placeholder.com/320x210" alt="placeholder" />
+          </div>
+        </div>
+        <p class="tag">${byline}</p>
     </div>`;
+
     storiesContent.appendChild(card);
   }
 }
 
-function generateContent(data) {
+function generateCardsContent(data) {
   const content = data.page.content;
 
   content.map((contentItem) => {
